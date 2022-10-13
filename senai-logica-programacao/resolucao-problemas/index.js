9fdf2534e2446c40e10e65d667259d2ebd7b9fb5
Paulo B Pessoa <ppessoarj@gmail.com>
2022-10-13 16:46:37 -0300

    Alteração para treino de loop

--- senai-logica-programacao/resolucao-problemas/index.js
+++ senai-logica-programacao/resolucao-problemas/index.js
@@ -1,14 +1,21 @@
 //variável, seu valor pode ser alterado no decorrer do programa
-let nomeEstudante = "Helena";
-console.log(nomeEstudante);
+//let nomeEstudante = "Helena";
+//console.log(nomeEstudante);
 
 //constante, seu valor não pode ser alterado
-const nomeDoEstudante = "Helena";
-console.log(nomeDoEstudante);
+//const nomeDoEstudante = "Helena";
+//console.log(nomeDoEstudante);
 
 //Em outras linguagens, portanto você encontrará algo semelhante a:
-String: nomeEstudante = "Helena";
+//String: nomeEstudante = "Helena";
 
+//Exercicio de lista
 let listaDeEstudantes = ["Helena", "Chico", "Mário"];
 let    quantidadeDeEstudantes =  listaDeEstudantes.length;
 console.log(quantidadeDeEstudantes);
+
+//Exercicio de loop
+for (let indice = 0; indice < quantidadeDeEstudantes;     indice++) { 
+    const alunoCorrente = listaDeEstudantes[indice];
+    console.log(alunoCorrente);
+    }

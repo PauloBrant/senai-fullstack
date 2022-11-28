b3bb6179ebf48cc08741f3f02865d5d81b107353
Paulo B Pessoa <ppessoarj@gmail.com>
2022-11-27 21:19:10 -0300

    final do dia 271122

--- UC9 - Codificacao Front/SA2/EPlayers/js/menuham.js
+++ UC9 - Codificacao Front/SA2/EPlayers/js/menuham.js
@@ -1,8 +1,10 @@
 const btnCelular = document.getElementById('btn-celular');
 
-function alteraMenu() {
+function alteraMenu(event) {
+    if(event.type === 'touchstart') event.preventDefault();
     const nav = document.getElementById('nav');
     nav.classList.toggle('active');
 }
 
 btnCelular.addEventListener('click',alteraMenu);
+btnCelular.addEventListener('touchstart',alteraMenu);
